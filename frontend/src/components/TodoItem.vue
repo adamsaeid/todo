41d4820f0be516f24a2d5fd60cdbb139f90cc1ca
Adam Saeid <adam.saeid@gmail.com>
2018-03-14 22:19:01 +0000

    Add text styles for item statuses

--- frontend/src/components/TodoItem.vue
+++ frontend/src/components/TodoItem.vue
@@ -1,12 +1,33 @@
 <template>
     <li>
-        {{ desc }} ({{ status }})
+        <span class="item-text" :class="itemTextClassObj">{{ desc }} ({{ status }})</span>
     </li>
 </template>
 
 <script>
 export default {
   name: 'TodoItem',
-  props: ['desc', 'status']
+  props: ['desc', 'status'],
+  computed: {
+    itemTextClassObj: function () {
+      return {
+        'todo-text': this.status === 'todo',
+        'doing-text': this.status === 'doing',
+        'done-text': this.status === 'done'
+      }
+    }
+  }
 }
 </script>
+
+<style>
+.todo-text {
+    color: black;
+}
+.doing-text {
+    color: orange;
+}
+.done-text {
+    color: green;
+}
+</style>
--- frontend/test/unit/specs/TodoItem.spec.js
+++ frontend/test/unit/specs/TodoItem.spec.js
@@ -1,15 +1,35 @@
 import { mount } from 'vue-test-utils'
 import TodoItem from '@/components/TodoItem'
 
-describe('TodoItem.vue', () => {
-  it('should render correct contents', () => {
-    const wrapper = mount(TodoItem, {
-      propsData: {
-        desc: 'Walk the dog',
-        status: 'todo'
-      }
-    })
+function mountItem (status) {
+  const wrapper = mount(TodoItem, {
+    propsData: {
+      desc: 'Walk the dog',
+      status: status
+    }
+  })
 
-    expect(wrapper.html()).to.contain('Walk the dog (todo)')
+  return wrapper
+}
+
+const todoItem = mountItem('todo')
+const doingItem = mountItem('doing')
+const doneItem = mountItem('done')
+
+describe('TodoItem', () => {
+  it('should render correct contents', () => {
+    expect(doneItem.find('.item-text').text()).to.contain('Walk the dog (done)')
+  })
+
+  it('sets todo-text class when status is "todo"', () => {
+    expect(todoItem.find('.item-text').classes()).to.contain('todo-text')
+  })
+
+  it ('sets doing-text class when status is "doing"', () => {
+    expect(doingItem.find('.item-text').classes()).to.contain('doing-text')
+  })
+
+  it('sets done-text class when status is "done"', () => {
+    expect(doneItem.find('.item-text').classes()).to.contain('done-text')
   })
 })
