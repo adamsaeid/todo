e56fc0ce136863c13dbf181bac4818f3efd443f7
Adam Saeid <adam.saeid@gmail.com>
2018-03-14 23:33:52 +0000

    Add todo/doing/done buttons

--- frontend/src/components/TodoItem.vue
+++ frontend/src/components/TodoItem.vue
@@ -1,13 +1,16 @@
 <template>
     <li>
         <span class="item-text" :class="itemTextClassObj">{{ desc }} ({{ status }})</span>
+        <button class="todo-btn" v-on:click="setTodo">todo</button>
+        <button class="doing-btn" v-on:click="setDoing">doing</button>
+        <button class="done-btn" v-on:click="setDone">done</button>
     </li>
 </template>
 
 <script>
 export default {
   name: 'TodoItem',
-  props: ['desc', 'status'],
+  props: ['id', 'desc', 'status'],
   computed: {
     itemTextClassObj: function () {
       return {
@@ -15,6 +18,17 @@ export default {
         'doing-text': this.status === 'doing',
         'done-text': this.status === 'done'
       }
+    }
+  },
+  methods: {
+    setTodo: function () {
+      this.$emit('statusChanged', {id: this.id, newStatus: 'todo'})
+    },
+    setDoing: function () {
+      this.$emit('statusChanged', {id: this.id, newStatus: 'doing'})
+    },
+    setDone: function () {
+      this.$emit('statusChanged', {id: this.id, newStatus: 'done'})
     }
   }
 }
--- frontend/test/unit/specs/TodoItem.spec.js
+++ frontend/test/unit/specs/TodoItem.spec.js
@@ -1,11 +1,13 @@
 import { mount } from 'vue-test-utils'
 import TodoItem from '@/components/TodoItem'
+import sinon from 'sinon'
 
 function mountItem (status) {
   const wrapper = mount(TodoItem, {
     propsData: {
+      id: 1,
       desc: 'Walk the dog',
-      status: status
+      status: status || 'todo'
     }
   })
 
@@ -25,11 +27,64 @@ describe('TodoItem', () => {
     expect(todoItem.find('.item-text').classes()).to.contain('todo-text')
   })
 
-  it ('sets doing-text class when status is "doing"', () => {
+  it('sets doing-text class when status is "doing"', () => {
     expect(doingItem.find('.item-text').classes()).to.contain('doing-text')
   })
 
   it('sets done-text class when status is "done"', () => {
     expect(doneItem.find('.item-text').classes()).to.contain('done-text')
+  })
+
+  describe('setTodo()', () => {
+    it('should be called when "todo" button clicked', () => {
+      var wrapper = mountItem()
+      var setTodoStub = sinon.stub()
+      wrapper.setMethods({setTodo: setTodoStub})
+
+      wrapper.find('button.todo-btn').trigger('click')
+      expect(setTodoStub.called).to.equal(true)
+    })
+
+    it('should emit correct statusChanged event', () => {
+      var wrapper = mountItem()
+      wrapper.find('button.todo-btn').trigger('click')
+      expect(wrapper.emitted().statusChanged[0]).to.deep.equal([{id: 1, newStatus: 'todo'}])
+    })
+  })
+
+  describe('setDoing()', () => {
+    it('should be called when "doing" button clicked', () => {
+      var wrapper = mountItem()
+
+      var setDoingStub = sinon.stub()
+      wrapper.setMethods({setDoing: setDoingStub})
+
+      wrapper.find('button.doing-btn').trigger('click')
+      expect(setDoingStub.called).to.equal(true)
+    })
+
+    it('should emit correct statusChanged event', () => {
+      var wrapper = mountItem()
+      wrapper.find('button.doing-btn').trigger('click')
+      expect(wrapper.emitted().statusChanged[0]).to.deep.equal([{id: 1, newStatus: 'doing'}])
+    })
+  })
+
+  describe('setDone()', () => {
+    it('should be called when "done" button clicked', () => {
+      var wrapper = mountItem()
+
+      var setDoneStub = sinon.stub()
+      wrapper.setMethods({setDone: setDoneStub})
+
+      wrapper.find('button.done-btn').trigger('click')
+      expect(setDoneStub.called).to.equal(true)
+    })
+
+    it('should emit correct statusChanged event', () => {
+      var wrapper = mountItem()
+      wrapper.find('button.done-btn').trigger('click')
+      expect(wrapper.emitted().statusChanged[0]).to.deep.equal([{id: 1, newStatus: 'done'}])
+    })
   })
 })
