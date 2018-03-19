074b31d49a6368f6056ceef8f8f65efa062cfd43
Adam Saeid <adam.saeid@gmail.com>
2018-03-19 22:56:22 +0000

    Use sauce labs for e2e tests

--- frontend/test/e2e/nightwatch.conf.js
+++ frontend/test/e2e/nightwatch.conf.js
@@ -1,6 +1,8 @@
 require('babel-register')
 var config = require('../../config')
 
+const TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER;
+
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: ['test/e2e/specs'],
@@ -8,7 +10,7 @@ module.exports = {
   custom_assertions_path: ['test/e2e/custom-assertions'],
 
   selenium: {
-    start_process: true,
+    start_process: false,
     server_path: require('selenium-server').path,
     host: '127.0.0.1',
     port: 4444,
@@ -18,13 +20,29 @@ module.exports = {
   },
 
   test_settings: {
-    default: {
-      selenium_port: 4444,
-      selenium_host: 'localhost',
+    default : {
+      launch_url : 'http://localhost',
+      selenium_port  : 80,
+      selenium_host  : 'ondemand.saucelabs.com',
       silent: true,
+      screenshots : {
+        enabled : false,
+        path : ''
+      },
+      username: process.env.SAUCE_USERNAME,
+      access_key: process.env.SAUCE_ACCESS_KEY,
+      screenshots: {
+        enabled: false,
+        path: '',
+      },
+      desiredCapabilities: {
+        build: `build-${TRAVIS_JOB_NUMBER}`,
+        'tunnel-identifier': TRAVIS_JOB_NUMBER,
+      },
       globals: {
-        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
-      }
+        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port),
+        waitForConditionTimeout: 10000,
+      },
     },
 
     chrome: {
