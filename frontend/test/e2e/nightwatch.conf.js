5a9bd6013eb9f6d878f44a8416a1b88bb1b292fd
Adam Saeid <adam.saeid@gmail.com>
2018-03-22 20:32:34 +0000

    Add separate nightwatch config for travis/local

--- frontend/test/e2e/nightwatch.conf.js
+++ frontend/test/e2e/nightwatch.conf.js
@@ -1,64 +1,7 @@
-require('babel-register')
-var config = require('../../config')
-
 const TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER;
 
-// http://nightwatchjs.org/gettingstarted#settings-file
-module.exports = {
-  src_folders: ['test/e2e/specs'],
-  output_folder: 'test/e2e/reports',
-  custom_assertions_path: ['test/e2e/custom-assertions'],
-
-  selenium: {
-    start_process: false,
-    server_path: require('selenium-server').path,
-    host: '127.0.0.1',
-    port: 4444,
-    cli_args: {
-      'webdriver.chrome.driver': require('chromedriver').path
-    }
-  },
-
-  test_settings: {
-    default : {
-      launch_url : 'http://localhost',
-      selenium_port  : 80,
-      selenium_host  : 'ondemand.saucelabs.com',
-      silent: true,
-      screenshots : {
-        enabled : false,
-        path : ''
-      },
-      username: process.env.SAUCE_USERNAME,
-      access_key: process.env.SAUCE_ACCESS_KEY,
-      screenshots: {
-        enabled: false,
-        path: '',
-      },
-      desiredCapabilities: {
-        build: `build-${TRAVIS_JOB_NUMBER}`,
-        'tunnel-identifier': TRAVIS_JOB_NUMBER,
-      },
-      globals: {
-        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port),
-        waitForConditionTimeout: 10000,
-      },
-    },
-
-    chrome: {
-      desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
-    },
-
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
-    }
-  }
+if (TRAVIS_JOB_NUMBER) {
+  module.exports = require('./nightwatch.conf.travis')
+} else {
+  module.exports = require('./nihtwatch.conf.local')
 }
